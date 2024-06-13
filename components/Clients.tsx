b8038baa0e02038857f3fdc13ae4e26acead5d97
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-06-12 19:25:03 -0700

    2:05:23 Sentry is set up and working on testimonials

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/Infinite-Moving-Cards'
+import {companies, testimonials} from "@/data"
 
 const Clients = () => {
   return (
@@ -8,8 +10,24 @@ const Clients = () => {
         <span className = "text-yellow-100">peers and family!</span>
         </h1>
 
-        <div className = "flex flex-wrap items-center ">
-            
+        <div className = "flex flex-col items-center max-lg:mt-10">
+            <InfiniteMovingCards 
+                items = {testimonials}
+                direction = "right"
+                speed = "slow"
+            />
+
+           {/* <div className = "flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+                {companies.map(({id, img, name, nameImg}) => (
+                    <div key = {id} className = "flex md:max-w-60 max-w-32 gap-2">
+                        <img 
+                            src = {nameImg}
+                            alt = {name}
+                            className = "md:w-24 w-20"
+                        />
+                    </div>
+                ))}
+            </div> */}
         </div>
     </div>
   )
--- components/ui/Infinite-Moving-Cards.tsx
+++ components/ui/Infinite-Moving-Cards.tsx
@@ -64,7 +64,7 @@ export const InfiniteMovingCards = ({
       } else if (speed === "normal") {
         containerRef.current.style.setProperty("--animation-duration", "40s");
       } else {
-        containerRef.current.style.setProperty("--animation-duration", "80s");
+        containerRef.current.style.setProperty("--animation-duration", "120s");
       }
     }
   };
@@ -72,24 +72,24 @@ export const InfiniteMovingCards = ({
     <div
       ref={containerRef}
       className={cn(
-        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20  max-w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
         ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+          " flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[80vw] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-white/[0.2] p-5 md:p-16 md:w-[60vw]"
             style={{
               background:
-                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
+                "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(9,9,9,1) 62%, rgba(0,0,0,1) 100%)",
             }}
             key={item.name}
           >
@@ -98,17 +98,22 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+              <span className=" relative z-20 sm:text-sm text-lg leading-[1.6] text-white font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
-                  </span>
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
-                  </span>
+                    <div className = "me-3">
+                        <img src = '/profile.svg' alt = "profile" />
+                    </div>
+                    <div className = "flex flex-col gap-1">
+                        <span className=" text-lg leading-[1.6] text-purple font-bold">
+                            {item.name}
+                        </span>
+                        <span className=" text-md leading-[1.6] text-purple font-bold">
+                            {item.title}
+                        </span>
+                    </div>
                 </span>
               </div>
             </blockquote>
