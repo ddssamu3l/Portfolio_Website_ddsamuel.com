e8b28b1d6f185b550279bf47b18ef5d9e7635120
ddssamu3l <72890797+ddssamu3l@users.noreply.github.com>
2024-06-14 00:30:20 -0700

    fixed mobile floating nav bar not appearing bug, redesigned page layouts and fixed margin bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,19 +10,10 @@ const Footer = () => {
         window.open(url, '_blank');
     }
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
+    <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw] mb-10">
-          I'm <span className="text-yellow-100">always</span> rechable with an email
+          I'm <span className="text-yellow-100">always rechable</span> with an email
         </h1>
        
         <a href="mailto:samueldeng78@gmail.com">
